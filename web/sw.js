aa4cdd14d27c0b9ceb242d1c9f8e440182a6d5c1
Mcaulee Maddison <McauleeMaddison@users.noreply.github.com>
2026-08-05 13:07:09 +0100

    Publish SkyPulse friend beta

---
+++ web/sw.js
@@ -0,0 +1,28 @@
+const CACHE = "skypulse-web-v2";
+const CORE = [
+  "./", "./index.html", "./privacy.html", "./styles.css", "./game.js", "./manifest.webmanifest",
+  "../assets/images/branding/skypulse-app-icon.png",
+  "../assets/images/backgrounds/neon-city.png",
+  "../assets/images/characters/nova.png", "../assets/images/characters/nova-flap.png",
+  "../assets/audio/flap.wav", "../assets/audio/score.wav", "../assets/audio/crash.wav",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => event.waitUntil(
+  caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())
+));
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  })));
+});
