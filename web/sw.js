691817dac5b71f004aeffab6b2a9f3d8bc3bd5b5
Mcaulee Maddison <McauleeMaddison@users.noreply.github.com>
2026-08-05 13:58:03 +0100

    Add premium visual polish

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "skypulse-web-v5";
+const CACHE = "skypulse-web-v6";
 const CORE = [
   "./", "./index.html", "./privacy.html", "./styles.css", "./game.js", "./manifest.webmanifest",
   "../assets/images/branding/skypulse-app-icon.png",
